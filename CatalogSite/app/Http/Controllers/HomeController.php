1b28244b05ba661957b658589c6a091881772597
NIKOLAI <zhanenev6@gmail.com>
2024-07-06 12:48:41 +0300

    Created new routes and changed links in nav bar

---
+++ CatalogSite/app/Http/Controllers/HomeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function home(){
+        return view('index'); 
+    }
+    
+    public function aboutus(){
+        return view('zanas'); 
+    }
+
+    public function partners(){
+        return view('partniori'); 
+    }
+
+    public function clients(){
+        return view('clienti'); 
+    }
+
+    public function service(){
+        return view('serviz'); 
+    }
+
+    public function contacts(){
+        return view('contacti'); 
+    }
+
+    public function categories(){
+        return view('kategorii'); 
+    }
+}
--- CatalogSite/routes/web.php
+++ CatalogSite/routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [HomeController::class,'home']);
+
+Route::get('/aboutus', [HomeController::class,'aboutus']);
+
+Route::get('/clients', [HomeController::class,'clients']);
+
+Route::get('/partners', [HomeController::class,'partners']);
+
+Route::get('/service', [HomeController::class,'service']);
+
+Route::get('/contacts', [HomeController::class,'contacts']);
+
+Route::get('/categories', [HomeController::class,'categories']);
