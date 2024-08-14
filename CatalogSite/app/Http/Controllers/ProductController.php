b8d9925c921c88e8ca7f1850fd18752e17caac77
Zhan <zhanenev6@gmail.com>
2024-08-14 19:30:19 +0300

    Update ProductController.php
    remove searching by mmanufacturer_code

--- CatalogSite/app/Http/Controllers/ProductController.php
+++ CatalogSite/app/Http/Controllers/ProductController.php
@@ -3,15 +3,36 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Models\Product;
 use Illuminate\Http\Request;
+use Psr\Container\NotFoundExceptionInterface;
+use App\Models\Product;
+
 
 class ProductController extends Controller
 {
-    public function index($id){
+    public function index(){
+        return view('product'); 
+    }
 
-        $product = Product::find($id);
+    public function search(Request $request)
+    {
+        $query = $request->input('query');
+        
+        if (!$query) {
+            return redirect()->back()->with('error', 'Please enter a search term.');
+        }
 
-        return view('product', ['product' => $product]); 
+        $product = Product::where('title', 'LIKE', "%{$query}%")
+                            ->orWhere('sku', 'LIKE', "%{$query}%")
+                            ->first();
+
+        if (!$product) {
+            // return not found page 
+            return redirect()->back()->with('error', 'The product is missing');
+
+        }
+
+        return view('product', ['product' => $product]);
     }
 }
+
