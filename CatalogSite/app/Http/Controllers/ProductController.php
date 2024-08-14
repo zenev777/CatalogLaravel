1770f8e17432bf42a994557da5e97696c89f018a
Zhan <zhanenev6@gmail.com>
2024-08-14 19:15:01 +0300

    Create search function

--- CatalogSite/app/Http/Controllers/ProductController.php
+++ CatalogSite/app/Http/Controllers/ProductController.php
@@ -4,10 +4,34 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Psr\Container\NotFoundExceptionInterface;
 
 class ProductController extends Controller
 {
     public function index(){
         return view('product'); 
     }
+
+    public function search(Request $request)
+    {
+        $query = $request->input('query');
+        
+        if (!$query) {
+            return redirect()->back()->with('error', 'Please enter a search term.');
+        }
+
+        $product = Product::where('title', 'LIKE', "%{$query}%")
+                            ->orWhere('sku', 'LIKE', "%{$query}%")
+                            ->orWhere('manufacturer_code', 'LIKE', "%{$query}%")
+                            ->first();
+
+        if (!$product) {
+            // return not found page 
+            return redirect()->back()->with('error', 'The product is missing');
+
+        }
+
+        return view('product', ['product' => $product]);
+    }
 }
+
