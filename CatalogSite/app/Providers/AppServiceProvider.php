1dafb655dba5ed2069f6e023f0db06db86e7abcc
Zhan <zhanenev6@gmail.com>
2024-09-12 18:39:59 +0300

    Edit Category nav bar

--- CatalogSite/app/Providers/AppServiceProvider.php
+++ CatalogSite/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Category;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Sharing data with a specific view
+        View::composer('includes.nav', function ($view) {
+            $view->with('categories', Category::where('visible', true)
+            ->orderBy('position', 'asc')
+            ->get());
+        });
     }
 }
