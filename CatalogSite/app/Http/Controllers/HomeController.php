f99b4dd214918521418f60905377e5bb0621ef56
Zhan <zhanenev6@gmail.com>
2024-09-22 19:44:03 +0300

    Update HomeController.php
    returns the first home box and list with the other 3 boxes

--- CatalogSite/app/Http/Controllers/HomeController.php
+++ CatalogSite/app/Http/Controllers/HomeController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Models\HomepageBox;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
+    public function index()
+    {
 
         $products = Product::all();
 
@@ -17,22 +19,28 @@ class HomeController extends Controller
 
         $categoryConvect = Category::where('slug', '=', 'reciklirani-konvektomati')->first();
 
-        $subcategoriesWash = Category::where('parent_id', '=',$categoryWash->id)->get();
+        $subcategoriesWash = Category::where('parent_id', '=', $categoryWash->id)->get();
 
-        $subcategoriesConvect = Category::where('parent_id', '=',$categoryConvect->id)->get();
+        $subcategoriesConvect = Category::where('parent_id', '=', $categoryConvect->id)->get();
 
         $productsWash = Product::where('category_id', '=', $categoryWash->id)->get();
 
+        $homeboxeOne = HomepageBox::where('position', '=', 1)->first();
+
+        $homeboxes = HomepageBox::whereIn('position', [2, 3, 4])
+            ->orderBy('position', 'asc')
+            ->get();
+
         foreach ($subcategoriesWash as $subcategory) {
-        $productsWash = $productsWash->merge(Product::where('category_id', '=', $subcategory->id)->get());
-        }   
+            $productsWash = $productsWash->merge(Product::where('category_id', '=', $subcategory->id)->get());
+        }
 
         $productsConvect = Product::where('category_id', '=', $categoryConvect->id)->get();
 
         foreach ($subcategoriesConvect as $subcategory) {
-        $productsConvect = $productsConvect->merge(Product::where('category_id', '=', $subcategory->id)->get());
+            $productsConvect = $productsConvect->merge(Product::where('category_id', '=', $subcategory->id)->get());
         }
 
-        return view('index', ['products' => $products,  'productsConvect' => $productsConvect, 'productsWash' => $productsWash,]);
+        return view('index', ['products' => $products,  'productsConvect' => $productsConvect, 'productsWash' => $productsWash, 'homeboxeOne' => $homeboxeOne, 'homeboxes' => $homeboxes,]);
     }
 }
